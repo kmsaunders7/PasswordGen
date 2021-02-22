3f18da04232c97e1a33261b587d87916af1b214f
Kristin Saunders <kmsaunders7@gmail.com>
2021-02-22 12:20:50 -0600

    add Readme and screenshot

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # PasswordGen
+> Generate a custom password with the click of a button
+
+Following the user's prompts a password will be created...
+
+## SCREENSHOT
+
+![Image of Password Generator Webpage](/PasswordScreenshot.JPG)
+
+## Features
+
+User will follow the prompts and at the end will get a custom generated password unique to them. Prompts will include: special characters? numbers? CAPITAL letters? or lowercase? User selects what they want in their special password. If no item is selected the user will be notified. After the prompts are submitted a new password will be formatted for the user to view in the text box.
+
+
+## Link
+
+https://kmsaunders7.github.io/PasswordGen/
--- script.js
+++ script.js
@@ -1,22 +1,63 @@
 // create var array for special characters
-var specialCharacters = ["!", "@","#","$","%","^","&","*","(",")","_","+","=","-","?","~",".",",","<",">"]
+var symbols = ["!", "@","#","$","%","^","&","*","(",")","_","+","=","-","?","~",".",",","<",">"]
 // var for numeric characters
- var numericCharacters = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 // var array upper case characters
- var upperCaseCharacters = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M"]
+var upperCase = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M"]
 // var array lower case characters
- var lowerCaseCharacters = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"]
-
-
-//Prompts to ask user what they want in their password....special characters, numbers, upper and/or lowercase letters
-
-var specialChar = confirm('Would you like to include $pec!@l ch@r@cters? Click OK');
-var numericChar = confirm('Would you like to include numb3rs? Click OK');
-var lowerChar = confirm('Would you like to include lowercase lettering? Click OK');
-var upperChar = confirm('Would you like to inlcude UPPERCASE LETTERING? Click OK');
+var lowerCase = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"]
 
 //Ask how many digits they would like in their password...
+var length = parseInt(prompt("How long do you want your password? Between 8 and 128 characters"))
 
+//If less than 8 or greater than 128 alert user
+
+  if (length < 8 || length > 128) {
+  alert("Number selected must be between 8-128");
+  }
+  
+
+//Ask user what characters they want in their password with confirm statements...
+var includeSymbols = confirm('Would you like to include $pec!@l ch@r@cters? Click OK');
+
+var includeNumbers = confirm('Would you like to include numb3rs? Click OK');
+
+var includeLower = confirm('Would you like to include lowercase lettering? Click OK');
+
+var includeUpper = confirm('Would you like to inlcude UPPERCASE LETTERING? Click OK');
+
+// if user selects cancel on all the prompts... 
+if (
+  includeSymbols === false &&
+  includeNumbers === false &&
+  includeLower === false &&
+  includeUpper === false
+) {
+  alert("Must select at least one of the options");
+  end
+}
+// function to generate the password making a different variable, containing the confirmed characters user wants to include
+function generatePassword () {
+  var inclusion = ""
+    if (includeSymbols === true) {
+      inclusion += symbols
+    }
+    if (includeNumbers === true) {
+      inclusion += numbers
+    }
+    if (includeLower === true) {
+      inclusion += lowerCase
+    } 
+    if (includeUpper === true) {
+      inclusion += upperCase
+    }
+  var password = ""
+    for (var i = 0; i < length; i++) {
+      var randomCharacters = Math.floor(Math.random() * inclusion.length)
+      password += inclusion[randomCharacters]
+    }
+    return password
+}
 
 
 // Assignment Code
